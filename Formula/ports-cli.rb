2cb5fe06798c22c047df59ed31ef051fb2762e7a
givvemee <givvemeee@gmail.com>
2026-01-07 11:49:07 +0900

    updated new version

--- Formula/ports-cli.rb
+++ Formula/ports-cli.rb
@@ -2,7 +2,7 @@ class PortsCli < Formula
   desc "Show listening ports with process names and paths"
   homepage "https://github.com/givvemee/ports-cli"
   url "https://github.com/givvemee/ports-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "4cc874687f3de9c9ec06ff124f0926fb7fb3fa71bf0471f98a323cffd5f8774e"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   def install
